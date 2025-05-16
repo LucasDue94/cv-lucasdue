ca80a21e45e4ed8336d2d1976b8ccaf11f226bac
lucasduegtec <lfarias@egtec.tec.br>
2025-05-16 10:19:48 -0300

    feature: ajusta o header
    - adiciona svgs

--- src/app/pages/home/header/header.component.ts
+++ src/app/pages/home/header/header.component.ts
@@ -15,8 +15,9 @@ import {MenuItem} from 'primeng/api';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  readonly wppLink = `https://wa.me/5582999825788?text=Olá%20Lucas,%20gostei%20do%20seu%20perfil%20e%20gostaria%20de%20entrar%20em%20contato%20para%20discutir%20uma%20oportunidade.%20Você%20tem%20interesse%20em%20conversar%20sobre%20uma%20posição?`;
+  readonly wppLink = `https://wa.me/5582996988207?text=Olá%20Lucas,%20gostei%20do%20seu%20perfil%20e%20gostaria%20de%20entrar%20em%20contato%20para%20discutir%20uma%20oportunidade.%20Você%20tem%20interesse%20em%20conversar%20sobre%20uma%20posição?`;
   readonly linkedinLink = `https://www.linkedin.com/in/lucasdue/`
+  readonly mailLink = `mailto:lucasdue94@gmail.com?subject=Oportunidade%20de%20trabalho&body=Olá%20Lucas,%20gostei%20do%20seu%20perfil.%20Gostaria%20de%20entrar%20em%20contato.`
   items: MenuItem[] = [{
     label: 'Sobre',
     url: `sobre`,
