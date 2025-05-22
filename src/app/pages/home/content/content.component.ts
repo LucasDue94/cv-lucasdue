338e9c727d180f972e76c0c6711e0377685a0a5c
lucasduegtec <lfarias@egtec.tec.br>
2025-05-22 12:22:37 -0300

    fix: remove imports não usados

--- src/app/pages/home/content/content.component.ts
+++ src/app/pages/home/content/content.component.ts
@@ -1,14 +1,10 @@
-import { Component } from '@angular/core';
-import { Timeline } from 'primeng/timeline';
-import { Card } from 'primeng/card';
-import { Button } from 'primeng/button';
-import { CommonModule } from '@angular/common';
-import { ScrollPanel } from 'primeng/scrollpanel';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-content',
-  imports: [Timeline, Card, Button, CommonModule, ScrollPanel, RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss',
 })
