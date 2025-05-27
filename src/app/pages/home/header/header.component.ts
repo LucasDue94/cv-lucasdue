9790eb7209047cd31367f2a2ab75cc1df29222dc
lucasduegtec <lfarias@egtec.tec.br>
2025-05-26 21:09:29 -0300

    feature: adiciona informações de tecnologias

--- src/app/pages/home/header/header.component.ts
+++ src/app/pages/home/header/header.component.ts
@@ -2,11 +2,11 @@ import {Component} from '@angular/core';
 import {ButtonModule} from 'primeng/button';
 import {RouterLink} from '@angular/router';
 import {Menu} from 'primeng/menu';
-import {MenuItem} from 'primeng/api';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [ButtonModule, RouterLink, Menu],
+  imports: [ButtonModule, RouterLink, Menu, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
@@ -14,26 +14,31 @@ export class HeaderComponent {
   readonly wppLink = `https://wa.me/5582996988207?text=Olá%20Lucas,%20gostei%20do%20seu%20perfil%20e%20gostaria%20de%20entrar%20em%20contato%20para%20discutir%20uma%20oportunidade.%20Você%20tem%20interesse%20em%20conversar%20sobre%20uma%20posição?`;
   readonly linkedinLink = `https://www.linkedin.com/in/lucasdue/`;
   readonly mailLink = `mailto:lucasdue94@gmail.com?subject=Oportunidade%20de%20trabalho&body=Olá%20Lucas,%20gostei%20do%20seu%20perfil.%20Gostaria%20de%20entrar%20em%20contato.`;
-  items: MenuItem[] = [
+  items: any[] = [
     {
       label: 'Sobre',
       url: `sobre`,
+      disable: false
     },
     {
       label: 'Experiência',
       url: `experiencia`,
+      disable: false
     },
     {
       label: 'Tecnologias',
       url: `tecnologias`,
+      disable: false
     },
     {
       label: 'Projetos',
       url: `projetos`,
+      disable: true
     },
     {
       label: 'Hobbies',
       url: `hobbies`,
+      disable: true
     },
   ];
 }
--- src/app/pages/tecnologias/tecnologias.component.ts
+++ src/app/pages/tecnologias/tecnologias.component.ts
@@ -1,9 +1,96 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Rating} from 'primeng/rating';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-tecnologias',
-  imports: [],
+  imports: [CommonModule, Rating, FormsModule],
   templateUrl: './tecnologias.component.html',
   styleUrl: './tecnologias.component.scss',
 })
-export class TecnologiasComponent {}
+export class TecnologiasComponent {
+  techCategories = [
+    {
+      title: 'Linguagens',
+      items: [
+        {name: 'TypeScript', rating: 5},
+        {name: 'JavaScript', rating: 5},
+        {name: 'HTML', rating: 5},
+        {name: 'CSS', rating: 5},
+        {name: 'Java', rating: 2},
+        {name: 'PHP', rating: 2},
+      ]
+    },
+    {
+      title: 'Frameworks CSS',
+      items: [
+        {name: 'PrimeNG', rating: 5},
+        {name: 'Bootstrap', rating: 4},
+        {name: 'Material', rating: 4},
+        {name: 'Tailwind', rating: 3},
+      ]
+    },
+    {
+      title: 'Frameworks Front-end',
+      items: [
+        {name: 'Angular', rating: 5},
+        {name: 'React.js', rating: 3},
+      ]
+    },
+    {
+      title: 'Back-end',
+      items: [
+        {name: 'Node.js', rating: 3},
+        {name: 'Mysql', rating: 2},
+        {name: 'Postgres', rating: 2},
+        {name: 'MongoDB', rating: 2},
+      ]
+    },
+    {
+      title: 'DevOps',
+      items: [
+        {name: 'Git', rating: 4},
+        {name: 'CI/CD', rating: 3},
+        {name: 'Docker', rating: 3},
+      ]
+    },
+    {
+      title: 'Bibliotecas',
+      items: [
+        {name: 'DevExtreme', rating: 5},
+        {name: 'RxJS', rating: 4},
+        {name: 'NG Zorro', rating: 2}
+      ]
+    },
+    {
+      title: 'Testes',
+      items: [
+        {name: 'Jasmine', rating: 4},
+        {name: 'Karma', rating: 4}
+      ]
+    }, {
+      title: 'Metodologias e Processos',
+      items: [
+        {name: 'Scrum', rating: 5},
+        {name: 'Kanban', rating: 5}
+      ]
+    },
+    {
+      title: 'Outras tecnologias relevantes',
+      items: [
+        {name: 'Slack', rating: 5},
+        {name: 'Teams', rating: 5},
+        {name: 'Trello', rating: 4},
+        {name: 'REST APIs', rating: 4},
+        {name: 'ESLint', rating: 4},
+        {name: 'Prettier', rating: 4},
+        {name: 'Postman', rating: 3},
+        {name: 'OAuth / JWT', rating: 3},
+        {name: 'Storybook', rating: 3},
+        {name: 'Jira', rating: 3},
+        {name: 'AWS', rating: 2},
+      ]
+    }
+  ]
+}
