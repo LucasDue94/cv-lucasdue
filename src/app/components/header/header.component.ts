45155deafcbdd7bbf32f62c40897e618c30c199e
lucasduegtec <lfarias@egtec.tec.br>
2025-05-28 14:56:46 -0300

    fix: cria service para persistir menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {ButtonModule} from 'primeng/button';
 import {RouterLink} from '@angular/router';
 import {Menu} from 'primeng/menu';
 import {CommonModule} from '@angular/common';
 import {MenuItem} from 'primeng/api';
+import {MenuService} from '../../../services/menu.service';
 
 @Component({
   selector: 'app-header',
@@ -11,7 +12,7 @@ import {MenuItem} from 'primeng/api';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   readonly wppLink = `https://wa.me/5582996988207?text=Olá%20Lucas,%20gostei%20do%20seu%20perfil%20e%20gostaria%20de%20entrar%20em%20contato%20para%20discutir%20uma%20oportunidade.%20Você%20tem%20interesse%20em%20conversar%20sobre%20uma%20posição?`;
   readonly linkedinLink = `https://www.linkedin.com/in/lucasdue/`;
   readonly mailLink = `mailto:lucasdue94@gmail.com?subject=Oportunidade%20de%20trabalho&body=Olá%20Lucas,%20gostei%20do%20seu%20perfil.%20Gostaria%20de%20entrar%20em%20contato.`;
@@ -42,4 +43,25 @@ export class HeaderComponent {
       disabled: true
     },
   ];
+
+  menuService = inject(MenuService);
+  currentMenu = '';
+
+  ngOnInit() {
+    this.menuService.getMenu().subscribe(menu => {
+      this.currentMenu = menu;
+    })
+  }
+
+  isSelected(item: MenuItem) {
+    return this.currentMenu === item.label
+  }
+
+  setMenu(item: MenuItem) {
+    this.menuService.setMenu(item.label ?? '');
+  }
+
+  ngOnDestroy() {
+    this.menuService.clear();
+  }
 }
---
+++ src/services/menu.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+  currentMenu: BehaviorSubject<string> = new BehaviorSubject('Sobre');
+  private readonly STORAGE_KEY = 'menu';
+
+  constructor() {
+  }
+
+  setMenu(menu: string): void {
+    localStorage.setItem(this.STORAGE_KEY, menu);
+    this.currentMenu.next(menu)
+  }
+
+  getMenu(): Observable<string> {
+    return this.currentMenu.asObservable()
+  }
+
+  clear(): void {
+    localStorage.removeItem(this.STORAGE_KEY);
+  }
+
+}
+
